143d24c3517201633656f1f76a6e59554c45e1e1
Anuj <113153788+anujjha2001@users.noreply.github.com>
2026-05-10 17:52:21 +0530

    login form updated with react and google sheets

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -5,6 +5,25 @@ import { FaUser, FaLock, FaEnvelope } from "react-icons/fa";
 function AuthForm() {
   const [isLogin, setIsLogin] = useState(true);
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async () => {
+    const url = "https://script.google.com/macros/s/AKfycbxATJaL4JZWH7t6w1cI_LwIn41SW24r2oQgqv_Ma93uXxd7MeVzDqIIGt7_7uXJphPM/exec";
+
+    await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+
+    alert("Data saved successfully ");
+  };
+
   return (
     <div className="container">
 
@@ -18,23 +37,37 @@ function AuthForm() {
           {isLogin ? "Login to continue" : "Register to get started"}
         </p>
 
+        {/* NAME (only register) */}
         {!isLogin && (
           <div className="inputBox">
             <FaUser />
-            <input placeholder="Full Name" />
+            <input
+              placeholder="Full Name"
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
         )}
 
+        {/* EMAIL */}
         <div className="inputBox">
           <FaEnvelope />
-          <input placeholder="Email" />
+          <input
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
 
+        {/* PASSWORD */}
         <div className="inputBox">
           <FaLock />
-          <input type="password" placeholder="Password" />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
+        {/* CONFIRM PASSWORD (register only) */}
         {!isLogin && (
           <div className="inputBox">
             <FaLock />
@@ -42,10 +75,12 @@ function AuthForm() {
           </div>
         )}
 
-        <button className="btn">
+        {/* BUTTON */}
+        <button className="btn" onClick={handleSubmit}>
           {isLogin ? "Login" : "Register"}
         </button>
 
+        {/* SWITCH */}
         <p className="switch" onClick={() => setIsLogin(!isLogin)}>
           {isLogin
             ? "New here? Create account"
